7d9b06eb18d6640254362d1be08fe3d8108edc8d
Vittalius <Vittalius@users.noreply.github.com>
2023-10-05 19:52:34 +0300

    📝 used service for base templates

--- src/page/router.py
+++ src/page/router.py
@@ -1,13 +1,8 @@
-from auth import auth_service
 from auth.user_forms import AuthUserDataForm
-from database import Database, db_helper
 from fastapi import APIRouter, Request, Depends
-from models import User, RoleNameEnum
 from starlette.responses import HTMLResponse
-from starlette.templating import Jinja2Templates
-from users.schema import Token
-from users import user_service
 
+from .service import template_service, templates_name, RequestContext
 
 page_router = APIRouter(
 	tags=["Pages"],
@@ -15,47 +10,17 @@ page_router = APIRouter(
 	responses={404: {"description": "Not found"}},
 )
 
-templates = Jinja2Templates(directory="templates")
-
 
 @page_router.get("/", response_class=HTMLResponse)
 async def auth_page(request: Request):
-	return templates.TemplateResponse("auth_panel.html", context={"request": request})
+	return template_service.generate_template(templates_name.get('auth_panel'),
+	                                          RequestContext(request=request))
 
 
 @page_router.post('/user-page', response_class=HTMLResponse)
 async def user_page(
 		request: Request,
 		data_form: AuthUserDataForm = Depends(AuthUserDataForm.as_form),
-		db: Database = Depends(db_helper.session_dependency)
 ):
-	try:
-		token: Token = auth_service.get_access_token(db, data_form)
-		user: User = auth_service.get_user_by_token(db, token.access_token)
-
-		users = []
-		context = {
-			"request": request,
-			"token": token,
-			"user": user
-		}
-		for user_role in user.roles:
-			if RoleNameEnum.ADMIN.value == user_role.name:
-				users = user_service.get_all_users()
-				break
-
-		if not users:
-			return templates.TemplateResponse('user_panel.html', context=context)
-
-		return templates.TemplateResponse('user_panel.html', context={
-			"request": request,
-			"token": token,
-			"user": user,
-			"users": users
-		})
-
-	except Exception as err:
-		return templates.TemplateResponse('error_panel.html', context={
-			"request": request,
-			"error": err.args[0]
-		})
+	return template_service.generate_user_page_template(templates_name.get('user_panel'),
+	                                                    dict(request=request, data=data_form))
