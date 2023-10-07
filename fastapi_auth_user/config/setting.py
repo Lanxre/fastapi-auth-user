06b765c8f4fb4d349cf1f9fc5adfdcda4ed6b487
Vittalius <Vittalius@users.noreply.github.com>
2023-10-07 15:10:55 +0300

    🔧 use os.getenv() for default value

--- fastapi_auth_user/config/setting.py
+++ fastapi_auth_user/config/setting.py
@@ -1,3 +1,5 @@
+import os
+
 from dotenv import load_dotenv, find_dotenv
 from pydantic import BaseSettings
 
@@ -5,20 +7,26 @@ load_dotenv(find_dotenv())
 
 
 class Settings(BaseSettings):
-	DB_USER: str | None
-	DB_PASSWORD: str | None
-	DB_HOST: str | None
-	DB_NAME: str | None
-	DATABASE_URL: str
+	DB_USER: str | None = os.getenv("DB_USER")
+	DB_PASSWORD: str | None = os.getenv("DB_PASSWORD")
+	DB_HOST: str | None = os.getenv("DB_HOST")
+	DB_NAME: str | None = os.getenv("DB_NAME")
+	DATABASE_URL: str = os.getenv("DATABASE_URL")
 
-	SECRET_KEY: str
-	ALGORITHM: str
+	SECRET_KEY: str = os.getenv("SECRET_KEY")
+	ALGORITHM: str = os.getenv("ALGORITHM")
 
 	def get_db_url(self) -> str:
 		"""
 		Return: url for connect database by .env variable
 		"""
 		return self.DATABASE_URL
+
+	class Config:
+		env_prefix: str = ""
+		case_sensitive: bool = False
+		env_file: str = ".env"
+		env_file_encoding: str = "utf-8"
 
 
 settings = Settings()
