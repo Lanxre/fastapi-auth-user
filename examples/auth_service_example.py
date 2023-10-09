2311bd02e091b06bf30e7a64fa54fda929ecf600
Vittalius <Vittalius@users.noreply.github.com>
2023-10-09 15:41:35 +0300

    🔧 update example auth

--- examples/auth_service_example.py
+++ examples/auth_service_example.py
@@ -1,9 +1,22 @@
-from fastapi_auth_user.database import db_helper
-from fastapi_auth_user.auth.user_forms import AuthUserDataForm
+from typing import Optional
+
+from pydantic import BaseModel
+
 from fastapi_auth_user.auth import auth_service
 from fastapi_auth_user.users.schema import Token
 
+
+class AuthUserData(BaseModel):
+	email: Optional[str] = None
+	password: str = ...
+	username: Optional[str] = None
+
+
 if __name__ == "__main__":
-	user_data: AuthUserDataForm = AuthUserDataForm()
-	token: Token = auth_service.get_access_token(db_helper.session_dependency(), user_data)
-	user = auth_service.get_user_by_token(db_helper.session_dependency(), token)
+	user_data: AuthUserData = AuthUserData(
+		email="user@example.com",
+		password="password123",
+		username="username123"
+	)
+	token: Token = auth_service.get_access_token(user_data)
+	user = auth_service.get_user_by_token(token)
