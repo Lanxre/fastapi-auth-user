0071fb4d798abf1a146ef48d4dfd0b50a409ee32
Vittalius <Vittalius@users.noreply.github.com>
2023-10-10 22:56:39 +0300

    🔧 add __repr__

--- fastapi_auth_user/auth/permissions.py
+++ fastapi_auth_user/auth/permissions.py
@@ -4,10 +4,11 @@ from fastapi import Depends, HTTPException, status
 
 from .exception import PermissionException
 from .service import AuthenticationService
-from ..database import Database, db_helper
+from ..database import db_helper
 from ..models import RoleNameEnum, User
 
 auth_service = AuthenticationService(next(db_helper.session_dependency()))
+
 
 class RolePermissions:
 	def __init__(self, roles: List[RoleNameEnum]):
@@ -18,16 +19,18 @@ class RolePermissions:
 			token: str = Depends(auth_service.oauth2_scheme)
 	) -> Union[bool, PermissionException]:
 		try:
-
 			current_user: User = auth_service.get_user_by_token(token)
 			for role in self.roles:
 				for user_role in current_user.roles:
 					if role.value == user_role.name:
 						return True
 			else:
-				raise PermissionException(message=f'Permission denied', role=self.roles[0])
+				raise PermissionException(message=f'Permission denied', role=str(self))
 		except PermissionException as err:
 			raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail=str(err))
 
-		except Exception as err:
+		except Exception:
 			raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail="Permission denied")
+
+	def __repr__(self) -> str:
+		return f"RolePermissions([{','.join(role.value for role in self.roles)}])"
