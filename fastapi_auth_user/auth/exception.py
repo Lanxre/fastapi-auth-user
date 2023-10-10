5a0c1034a958a59aa481d12a5dded29a3f18af2c
Vittalius <Vittalius@users.noreply.github.com>
2023-10-10 22:55:49 +0300

    🔧 str role param

--- fastapi_auth_user/auth/exception.py
+++ fastapi_auth_user/auth/exception.py
@@ -2,7 +2,7 @@ from fastapi_auth_user.models import RoleNameEnum
 
 
 class PermissionException(Exception):
-	def __init__(self, message: str, role: RoleNameEnum):
+	def __init__(self, message: str, role: str):
 		self.message = message
 		self.role = role
 		super().__init__(self.message)
